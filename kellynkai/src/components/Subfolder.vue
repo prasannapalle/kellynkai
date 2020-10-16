2af1a70839d72b8db1236b1c7b42711ce0ed4396
prasannapalle <laxmiprasanna.palle@sjsu.edu>
2020-10-16 02:25:16 -0700

    changes

---
+++ kellynkai/src/components/Subfolder.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>
+              <img  class="folder" src="../assets/folder-regular.svg" width=100px height=100px alt="Kiwi standing on oval">
+                       <img class="folder" src="../assets/folder-regular.svg" width=100px height=100px alt="Kiwi standing on oval">
+                  <img class="folder" src="../assets/folder-regular.svg" width=100px height=100px alt="Kiwi standing on oval">
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    name:'subfolder'
+}
+</script>
--- kellynkai/src/main.js
+++ kellynkai/src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import VueRouter from 'vue-router'
+import subfolder from './components/Subfolder'
+import HelloWorld from './components/HelloWorld'
+Vue.use(VueRouter);
 Vue.config.productionTip = false
-
+const routes = [
+  {path:'/',component:HelloWorld},
+  {path:'/subfolder',component:subfolder}
+]
+const router = new VueRouter({
+  routes
+})
 new Vue({
+  router:router,
   render: h => h(App),
 }).$mount('#app')
